b80d4d84f1de105608850b6838440dfd908ed725
alsafa012 <alsafa012@gmail.com>
2023-08-30 03:04:57 +0600

    almost complete

--- my.js
+++ my.js
@@ -1,28 +1,28 @@
 function start() {
-     let num = 0;
+     let num = 0 ;
      // let second = 0;
-     // let minutes = 0;
-     // let hours = 0;
-     let second = text("sec");
-     let minutes = text("min");
-     let hours = text("hour");
+     // let minutes = '00';
+     // let hours = '00';
+     let second = text("sec").innerText='00';
+     let minutes = text("min").innerText='00';
+     let hours = text("hour").innerText='00';
      const interval = setInterval(() => {
           const n = ++num;
           //  num = second + n;
-          if (n === 10) {
-               num = 0;              
+          if (num === 10) {
+               num = '00';
           }
-          if (num === 0) {
+          if (num === '00') {
               minutes++;
           }
           if(minutes === 2){
-               minutes=0;
+               minutes='00';
                hours++;
           }
-          display("sec", num);
+          
           display("min", minutes);
+          display("sec", num);
           display("hour", hours);
-
 
           // stop
           const stop = document.getElementById("stop");
@@ -41,4 +41,9 @@ function text(id) {
 function display(elementId, value) {
      const element = document.getElementById(elementId);
      element.innerText = value;
+}
+function reset(){
+     document.getElementById('hour').innerText = '00';
+     document.getElementById('min').innerText = '00';
+     document.getElementById('sec').innerText = '00';
 }
